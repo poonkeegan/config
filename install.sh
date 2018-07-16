dbaf480be0e287fdc4812b1efc3ea16f0d914451
Keegan Poon <poonkeegan@gmail.com>
2018-07-16 18:43:51 +0000

    Add install

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -s vimrc ~/.vimrc
+ln -s tmux.conf ~/.tmux.conf
+ln -s bashrc ~/.bashrc
+ln -s bash_profile ~/.bash_profile
+ln -s Xresources ~/.Xresources
+mkdir -p ~/.config/ranger
+ln -s rifle.conf ~/.config/ranger/rifle.conf
